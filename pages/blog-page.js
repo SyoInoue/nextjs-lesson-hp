b84ce776586aa6a22c76c33f44086f71151a2cbd
SyoInoue <funnyfacee7777@gmail.com>
2021-03-18 21:30:57 +0900

    initial commit

---
+++ pages/blog-page.js
@@ -0,0 +1,23 @@
+import Layout from "../components/Layout";
+import Post from "../components/Post";
+import { getAllPostsData } from "../lib/posts"; //libで取ってきたデータのインポート
+
+const Blog = ({ posts }) => {
+  return ( //postsをmap関数で繰り返し処理する。
+    <Layout title="Blog">
+      <ul className="m-10">
+        {posts && posts.map((post) => <Post key={post.id} post={post} />)}
+      </ul>
+    </Layout>
+  );
+};
+
+export default Blog;
+
+//build時にサーバーサイドで実行される仕組み。
+export async function getStaticProps() { //getStaticPropsはNest.jsの関数
+  const posts = await getAllPostsData();
+  return {
+    props: { posts }, //propsは親から子に情報を渡す。
+  };
+};
